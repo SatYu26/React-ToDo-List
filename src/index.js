0e1f650f24944b5c2fa39407c9c572e2110ae2d7
SatYu26 <goyalsatyam8@gmail.com>
2021-03-03 22:00:13 +0530

    Persisting the Redux store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { persistStore } from "redux-persist";
+import { PersistGate, persistGate } from "redux-persist/lib/integration/react";
 import { Provider } from "react-redux";
 import { configureStore } from "./store.js";
 import "./index.css";
 import App from "./App";
 
+const store = configureStore();
+const persistor = persistStore(store);
+
 ReactDOM.render(
-  <Provider store={configureStore()}>
-    <App />
+  <Provider store={store}>
+    <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
