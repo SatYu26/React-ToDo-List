971ecb87565c35dd619084271cb31c7990bcb1fc
SatYu26 <goyalsatyam8@gmail.com>
2021-03-04 23:04:19 +0530

    refractoring the todos reducer

--- src/todos/reducers.js
+++ src/todos/reducers.js
@@ -47,6 +47,16 @@ export const todos = (state = [], action) => {
         return todo;
       });
     }
+    case LOAD_TODOS_IN_PROGRESS: {
+      return state;
+    }
+    case LOAD_TODOS_SUCCESS: {
+      const { todos } = payload;
+      return todos;
+    }
+    case LOAD_TODOS_FAILURE: {
+      return state;
+    }
     default:
       return state;
   }
