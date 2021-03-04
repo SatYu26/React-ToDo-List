204abb709cae52c14524e291cccef8f79aeff729
SatYu26 <goyalsatyam8@gmail.com>
2021-03-04 20:42:15 +0530

    todos API and async thunks

--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
 import { todos } from "./todos/reducers.js";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const reducers = { todos };
 
@@ -17,7 +19,4 @@ const rootReducer = combineReducers(reducers);
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const configureStore = () =>
-  createStore(
-    persistedReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  );
+  createStore(persistedReducer, composeWithDevTools(applyMiddleware(thunk)));
---
+++ src/todos/thunks.js
@@ -0,0 +1,22 @@
+import {
+  loadTodosFailure,
+  loadTodosInProgress,
+  loadTodosSuccess,
+} from "./actions.js";
+
+export const loadTodos = () => async (dispatch, getState) => {
+  try {
+    dispatch(loadTodosInProgress());
+    const response = await fetch("http://localhost:8080/todos");
+    const todos = await response.json();
+
+    dispatch(loadTodosSuccess(todos));
+  } catch (e) {
+    dispatch(loadTodosFailure());
+    dispatch(displayAlert(e));
+  }
+};
+
+export const displayAlert = (text) => {
+  alert(text);
+};
